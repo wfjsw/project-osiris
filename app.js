4e2f89972b10585280236c5fc63e92af4d8b76bf
wfjsw <wfjsw@users.noreply.github.com>
2017-08-27 17:25:44 +0800

    Update to reflect latest API change.

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 #!/usr/bin/env Node
 
-'use strict';
-
 const Telegram = require('node-telegram-bot-api');
 const fs = require('fs');
 const config = require('./config.json');
 
 var bot = new Telegram(config['api-key'], config['options']);
 
-const message_type = ['text', 'audio', 'document', 'photo', 'sticker', 'video', 'voice', 'contact', 'location', 'venue', 'new_chat_member', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'pinned_message'];
-const inline_type = ['inline_query', 'chosen_inline_result', 'callback_query'];
+const message_type = [
+    'audio',
+    'channel_chat_created',
+    'contact',
+    'delete_chat_photo',
+    'document',
+    'game',
+    'group_chat_created',
+    'invoice',
+    'left_chat_member',
+    'location',
+    'migrate_from_chat_id',
+    'migrate_to_chat_id',
+    'new_chat_members',
+    'new_chat_photo',
+    'new_chat_title',
+    'photo',
+    'pinned_message',
+    'sticker',
+    'successful_payment',
+    'supergroup_chat_created',
+    'text',
+    'video',
+    'video_note',
+    'voice',
+]
+const other_type = ['edited_message', 'channel_post', 'inline_query', 'chosen_inline_result', 'callback_query', 'shipping_query', 'pre_checkout_query']
 var global_e = {
     bot: bot,
     me: {},
@@ -50,13 +73,13 @@ function loadAllLib() {
     libfiles.forEach((targetlib) => {
         var libname = getJsFilename(targetlib);
         if (!libname) return;
-        if (libname.slice(0,9).toLowerCase() == 'disabled_') return;
+        if (libname.slice(0, 9).toLowerCase() == 'disabled_') return;
 
         console.log(`Loading library ${targetlib} ...`);
         global_e.libs[libname] = require(`./lib/${targetlib}`);
-        if (global_e.libs[libname].init) 
-                global_e.libs[libname].init(global_e);
-    }) 
+        if (global_e.libs[libname].init)
+            global_e.libs[libname].init(global_e);
+    })
 }
 
 function loadAllPlugin() {
@@ -66,7 +89,7 @@ function loadAllPlugin() {
         var plgname = getJsFilename(targetplg);
 
         if (!plgname) return;
-        if (plgname.slice(0,9).toLowerCase() == 'disabled_') return;
+        if (plgname.slice(0, 9).toLowerCase() == 'disabled_') return;
 
         console.log(`Loading plugin ${targetplg} ...`);
         global_e.plugins[plgname] = require(`./plugins/${targetplg}`);
@@ -77,25 +100,25 @@ function loadAllPlugin() {
             global_e.plugins[plgname].run.forEach((runobj) => {
                 global_e.runners.push(runobj);
             })
-    }) 
+    })
 }
 
 function diverseListeners() {
     message_type.forEach((type) => {
         global_e.msgtype_listeners[type] = []; // init array
     });
-    inline_type.forEach((type) => {
+    other_type.forEach((type) => {
         global_e.msgtype_listeners[type] = []; // init array
     });
     global_e.edited_listeners = []; // init array
     global_e.runners.forEach(([test, callback]) => {
         // Diverse MediaType Listeners
-        if ((test.constructor == String) && ((message_type.indexOf(test) > -1) || (inline_type).indexOf(test) > -1))
+        if ((test.constructor == String) && ((message_type.indexOf(test) > -1) || (other_type).indexOf(test) > -1))
             global_e.msgtype_listeners[test].push(callback);
-        else if (test == 'edited_message') 
+        else if (test == 'edited_message')
             global_e.edited_listeners.push(callback);
         // Diverse RegExpression Listeners
-        else if (test instanceof RegExp) 
+        else if (test instanceof RegExp)
             global_e.regex_listeners.push([test, callback]);
     });
 }
@@ -106,7 +129,7 @@ diverseListeners();
 
 function trimCmd(command) {
     try {
-        if (command.slice(0,1) == '/') {
+        if (command.slice(0, 1) == '/') {
             var t1 = command.split(' ');
             var t2 = t1[0].split('@')
             if (t2.length > 1) {
@@ -131,26 +154,26 @@ function trimCmd(command) {
 bot.on('message', (msg) => {
     // Preprocessor hook all messages
     try {
-    global_e.preprocessors.forEach((preprocessor) => {
-        preprocessor(msg, bot);
-    });
+        global_e.preprocessors.forEach((preprocessor) => {
+            preprocessor(msg, bot);
+        });
 
-    // Process MediaTypes
-    message_type.forEach((type) => {
-        if (msg[type]) 
-            global_e.msgtype_listeners[type].forEach((cb) => {
-                cb(msg, type, bot);
-            });
-    });
-    
-    // Process RegExp
-    global_e.regex_listeners.forEach( ([test, callback]) => {
-        var matches;
-        if (msg.text) matches = test.exec(trimCmd(msg.text));
-        else if (msg.caption) matches = test.exec(msg.caption);
-        if (matches)
-            callback(msg, matches, bot);
-    });
+        // Process MediaTypes
+        message_type.forEach((type) => {
+            if (msg[type])
+                global_e.msgtype_listeners[type].forEach((cb) => {
+                    cb(msg, type, bot);
+                });
+        });
+
+        // Process RegExp
+        global_e.regex_listeners.forEach(([test, callback]) => {
+            var matches;
+            if (msg.text) matches = test.exec(trimCmd(msg.text));
+            else if (msg.caption) matches = test.exec(msg.caption);
+            if (matches)
+                callback(msg, matches, bot);
+        });
     } catch (e) {
         console.error(e);
     }
@@ -166,7 +189,7 @@ bot.on('edited_message', (msg) => {
     }
 });
 
-inline_type.forEach((type) => {
+other_type.forEach((type) => {
     bot.on(type, (msg) => {
         global_e.msgtype_listeners[type].forEach((cb) => {
             cb(msg, type, bot);
